2bda95b29529493c80d78d076c601d67cc7114f2
Daniel Klein <othercriteria@gmail.com>
2018-03-02 17:56:03 -0800

    Add command-line options (and fix regexp)

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const Twitter = require('twitter');
+const optionDefinitions = [
+    { name: 'dry-run', alias: 'd', type: Boolean, defaultValue: false },
+    { name: 'num-matches', alias: 'm', type: Number, defaultValue: 20 },
+    { name: 'interval', alias: 'i', type: Number, defaultValue: 5 }
+];
+const commandLineArgs = require('command-line-args');
+const options = commandLineArgs(optionDefinitions, { camelCase: true });
+console.log('Options:', options);
 
+const Twitter = require('twitter');
 const credentials = require('./credentials');
 const client = new Twitter(credentials);
 
@@ -8,14 +16,14 @@ const query_good = '"is good" -filter:retweets';
 
 // Being particular about capitalization and punctutation, since
 // I'll want to be able to easily reverse the statement.
-const re_good = /([A-Z])(([a-z]+) (\w+ ))*is good\./
+const re_good = /([A-Z])(\w+(\s\w+)+\s)is good\./
 
 const to_bad = (match =>
 		'What if ' + match[1].toLowerCase() + match[2] + 'is bad?');
 
 function searchAndTweet(succeed, fail) {
     client.get('search/tweets',
-	       { q: query_good, count: 100 },
+	       { q: query_good, count: options.numMatches },
 	       function(err, tweets, response) {
 		   if (!tweets.statuses) {
 		       fail(err);
@@ -24,6 +32,8 @@ function searchAndTweet(succeed, fail) {
 		   tweets.statuses.forEach(function(tweet) {
 		       const match = tweet.text.match(re_good);
 		       if (match) {
+			   console.log(tweet.text)
+
 			   const tweetId = tweet.id_str;
 			   const userId = tweet.user.id_str;
 			   const userName = tweet.user.screen_name;
@@ -35,14 +45,16 @@ function searchAndTweet(succeed, fail) {
 					  '/status/' +
 					  tweetId);
 
-			   // console.log(qtUrl, newTweet);
-
-			   client.post('statuses/update',
-				       { status: newTweet,
-					 attachment_url: qtUrl },
-				       function(err, tweet, response) {
-					   console.log(err || tweet.text);
-				       });
+			   if (options.dryRun) {
+			       console.log(qtUrl, newTweet);
+			   } else {
+			       client.post('statuses/update',
+					   { status: newTweet,
+					     attachment_url: qtUrl },
+					   function(err, tweet, response) {
+					       console.log(err || tweet.text);
+					   });
+			   }
 		       } else {
 			   // do nothing
 		       }
@@ -56,4 +68,4 @@ searchAndTweet(console.log, console.log);
 
 setInterval(function() {
     searchAndTweet(console.log, console.log);
-}, 5 * 60 * 1000);
+}, options.interval * 60 * 1000);
